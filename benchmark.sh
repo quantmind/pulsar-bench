4c178930d8337507ffc722dd950f6afe9f3d715c
Luca Sbardella <luca@quantmind.com>
2017-02-04 10:15:08 +0000

    net

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --network host --name bench-runner quantmind/pulsar-bench python3 bench.py $1
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --net host --name bench-runner quantmind/pulsar-bench python3 bench.py $1
