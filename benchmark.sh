cb4bd46fa8db8dfcc5fc0b08999d85e3b6da2f9f
Luca Sbardella <luca@quantmind.com>
2017-02-04 11:42:11 +0000

    use different directory for docker sock

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --net host --name bench-runner quantmind/pulsar-bench python3 bench.py $1
+docker run --rm \
+    -v /var/run/docker.sock:/home/benchmarking/docker.sock \
+    -e DOCKER_HOST=unix:///home/benchmarking/docker.sock \
+    --net host \
+    --name bench-runner \
+    quantmind/pulsar-bench \
+    python3 bench.py $1
--- circle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -v /var/run/docker.sock:/var/run/docker.sock --net host quantmind/pulsar-bench python3 bench.py $1
