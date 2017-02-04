7ff2e6ad23e905ffd6f9c86e0abb4b9ae4ddc4a1
Luca Sbardella <luca@quantmind.com>
2017-02-04 16:40:28 +0000

    fix benchmarks script

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,9 @@
 
 docker run --rm \
     -v /var/run/docker.sock:/home/benchmarking/docker.sock \
+    -v $PWD:/home/benchmarking/results \
     -e DOCKER_HOST=unix:///home/benchmarking/docker.sock \
     --net host \
     --name bench-runner \
     quantmind/pulsar-bench \
-    python3 bench.py $1
+    python3 bench.py "$@"
